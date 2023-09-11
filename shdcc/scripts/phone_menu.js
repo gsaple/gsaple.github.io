ce69487302123b1a1177ef10bce3a7b8bde967b9
gsaple <yymelau@gmail.com>
2023-09-11 22:50:38 +1000

    correct phone_menu.js location

--- shdcc/scripts/phone_menu.js
+++ shdcc/scripts/phone_menu.js
@@ -1,6 +1,6 @@
 const menuButton = document.getElementById("menu-button");
+const menu = document.querySelector(".phone-menu");
 function showMenu() {
-  const menu = document.querySelector(".phone-menu");
   if (menu.style.display === "flex") {
     menu.style.display = "none";
     menuButton.classList.remove("fa-window-close");
@@ -13,6 +13,20 @@ function showMenu() {
     menuButton.classList.add("fa-window-close");
   }
 }
+
+function updateIcon() {
+  const screenWidth = window.innerWidth;
+  if (screenWidth > 480) {
+    document.querySelector(".phone-menu").style.display = "none";
+    const menuButton = document.getElementById("menu-button");
+    menuButton.classList.remove("fa-window-close");
+    menuButton.classList.add("fa-navicon");
+  }
+}
 menuButton.addEventListener("click", () => {
   showMenu();
+});
+
+window.addEventListener("resize", () => {
+  updateIcon();
 });
